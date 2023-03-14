73fd11c3f31e73e0da7837913d7e8f518ffaa40c
SkiddledGitHub <36399055+Abstructor@users.noreply.github.com>
2023-03-14 20:18:17 +0700

    [Scripts/Build] Modularize plugins

--- scripts/build.js
+++ scripts/build.js
@@ -1,35 +1,28 @@
-import asar from '@electron/asar';
 import esbuild from 'esbuild';
 import { lessLoader } from 'esbuild-plugin-less';
-import fs from 'fs';
 import { join } from 'path';
+import { bundle, install, report, setup } from './plugins.js';
 import manifest from '../manifest.json' assert { type: 'json' };
 
-const cwd = process.cwd();
 const prod = process.argv.includes('--production');
-const noInstall = process.argv.includes('--no-install');
-const bundle = process.argv.includes('--bundle');
+const shouldInstall = !process.argv.includes('--no-install');
+const shouldBundle = process.argv.includes('--bundle');
 
-Object.defineProperties(globalThis, {
-  repluggedFolder: {
-    get: () => {
-      switch (process.platform) {
-        case 'win32':
-          return join(process.env.APPDATA || '', 'replugged');
-        case 'darwin':
-          return join(process.env.HOME || '', 'Library', 'Application Support', 'replugged');
-        default:
-          return process.env.XDG_CONFIG_HOME ? join(process.env.XDG_CONFIG_HOME, 'replugged') : join(process.env.HOME || '', '.config', 'replugged');
-      }
-    },
-  },
-  installDest: {
-    get: () => join(repluggedFolder, 'themes', manifest.id),
+Object.defineProperty(globalThis, 'repluggedFolder', {
+  get: () => {
+    switch (process.platform) {
+      case 'win32':
+        return join(process.env.APPDATA || '', 'replugged');
+      case 'darwin':
+        return join(process.env.HOME || '', 'Library', 'Application Support', 'replugged');
+      default:
+        return process.env.XDG_CONFIG_HOME ? join(process.env.XDG_CONFIG_HOME, 'replugged') : join(process.env.HOME || '', '.config', 'replugged');
+    }
   },
 });
 
 const common = {
-  absWorkingDir: cwd,
+  absWorkingDir: process.cwd(),
   bundle: true,
   logLevel: 'info',
   minify: prod,
@@ -38,40 +31,10 @@ const common = {
     lessLoader({
       paths: ['src', 'src/scheme', 'src/ui'],
     }),
-    {
-      name: 'setup',
-      setup: (build) =>
-        build.onStart(() => {
-          if (!fs.existsSync('bundle')) fs.mkdirSync('bundle');
-          if (!fs.existsSync('dist')) fs.mkdirSync('dist');
-          if (fs.existsSync('dist') && fs.readdirSync('dist').length)
-            fs.readdirSync('dist').forEach((name) => fs.rmSync(`dist/${name}`, { recursive: true }));
-          fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest));
-        }),
-    },
-    {
-      name: 'install',
-      setup: noInstall
-        ? () => void 0
-        : (build) =>
-            build.onEnd(() => {
-              if (fs.existsSync(installDest)) fs.rmSync(installDest, { recursive: true });
-              fs.cpSync('dist', installDest, { recursive: true });
-              console.log(`\x1b[1;36mbuild:\x1b[0m installed newly built version`);
-            }),
-    },
-    {
-      name: 'bundle',
-      setup: !bundle
-        ? () => void 0
-        : (build) =>
-            build.onEnd(() => {
-              asar.createPackage('dist', `bundle/${manifest.id}.asar`);
-              fs.copyFileSync('dist/manifest.json', `bundle/${manifest.id}.json`);
-
-              console.log(`\n\x1b[1;36mbuild:\x1b[0m bundled`);
-            }),
-    },
+    setup(shouldBundle, manifest),
+    install(shouldInstall, join(repluggedFolder, 'themes', manifest.id)),
+    bundle(shouldBundle, manifest),
+    report(),
   ],
   sourcemap: !prod,
   target: 'chrome91',
---
+++ scripts/plugins.js
@@ -0,0 +1,60 @@
+import fs from 'fs';
+import asar from '@electron/asar';
+
+export const setup = (bundle, manifest) => ({
+  name: 'setup',
+  setup: (build) =>
+    build.onStart(() => {
+      if (!fs.existsSync('dist')) {
+        console.log('\x1b[1;36mbuild:\x1b[0m creating dist folder');
+        fs.mkdirSync('dist');
+      }
+      if (fs.existsSync('dist') && fs.readdirSync('dist').length) {
+        console.log('\x1b[1;36mbuild:\x1b[0m cleaning dist folder');
+        fs.readdirSync('dist').forEach((name) => fs.rmSync(`dist/${name}`, { recursive: true }));
+      }
+      if (bundle && !fs.existsSync('bundle')) {
+        console.log('\x1b[1;36mbuild:\x1b[0m creating bundle folder');
+        fs.mkdirSync('bundle');
+      }
+
+      fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest));
+    }),
+});
+
+export const install = (enabled, installDest) => ({
+  name: 'install',
+  setup: !enabled
+    ? () => void 0
+    : (build) =>
+        build.onEnd(() => {
+          if (fs.existsSync(installDest)) fs.rmSync(installDest, { recursive: true });
+          fs.cpSync('dist', installDest, { recursive: true });
+          console.log(`\x1b[1;36mbuild:\x1b[0m installed newly built version`);
+        }),
+});
+
+export const bundle = (enabled, manifest) => ({
+  name: 'bundle',
+  setup: !enabled
+    ? () => void 0
+    : (build) =>
+        build.onEnd(() => {
+          asar.createPackage('dist', `bundle/${manifest.id}.asar`);
+          fs.copyFileSync('dist/manifest.json', `bundle/${manifest.id}.json`);
+
+          console.log(`\x1b[1;36mbuild:\x1b[0m bundled`);
+        }),
+});
+
+export const report = () => ({
+  name: 'report',
+  setup: (build) =>
+    build.onEnd((result) => {
+      console.log(
+        '\x1b[1;36mbuild:\x1b[0m finished with',
+        `${result.errors.length ? `\x1b[1;31m${results.errors.length}` : '\x1b[1;32m0'} error(s)\x1b[0m`,
+        `and ${result.warnings.length ? `\x1b[1;33m${results.warnings.length}` : `\x1b[1;32m0`} warning(s)\x1b[0m`,
+      );
+    }),
+});
